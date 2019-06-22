1c550976f5952bdc19f517b2feecad1ceaf02116
unknown <gustavocarvalhinha@gmail.com>
2019-06-22 12:57:53 -0300

    aula3

---
+++ ConsoleApp30/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Console.WriteLine("digite a marca do carro: ");
+            string Marca = Console.ReadLine();
+            Console.WriteLine("digite o pais de origem do carro: ");
+            string PaisOrigem = Console.ReadLine();
+            Console.WriteLine("digite a quantidade de portas do veiculo: ");
+            int QuantidadePortas = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("digite a capacidade de combustivel do seu carro: ");
+            double QuantidadeCombustivel = double.Parse(Console.ReadLine());
+            Console.WriteLine("digite se seu carro é flex com true ou false: ");
+            bool Flex = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("digite quantos quilometros seu carro faz por litro: ");
+            double KML = Convert.ToDouble(Console.ReadLine());
+            carro veiculo = new carro(Marca, PaisOrigem, QuantidadePortas, QuantidadeCombustivel, Flex, KML);
+            Console.WriteLine($"marca: {veiculo.Marca}  Pais de origem: {veiculo.PaisOrigem}   Quantidade de portas: {veiculo.QuantidadePortas}  capacidade de combustivel:  {veiculo.QuantidadeCombustivel}  carro flex:  {veiculo.Flex} \n fara: {veiculo.KML} por litro. sua autonomia sera de {veiculo.km} quilomestros com o tanque cheio");
+           
+        }
+    }
+}
---
+++ ConsoleApp30/carro.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp30
+{
+    public class carro 
+    {
+        public string Marca;
+        public string PaisOrigem;
+        public int QuantidadePortas;
+        public double QuantidadeCombustivel;
+        public bool Flex;
+        public double KML;
+        public double km;
+
+        public carro(string marca, string paisOrigem, int quantidadePortas, double quantidadecombustivel, bool flex, double kml)
+        {
+            Marca = marca;
+            PaisOrigem = paisOrigem;
+            QuantidadePortas = quantidadePortas;
+            QuantidadeCombustivel = quantidadecombustivel;
+            Flex = flex;
+            KML = kml;
+            km = kml * quantidadecombustivel;
+        }   
+    }
+}
